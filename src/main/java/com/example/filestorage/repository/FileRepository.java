9fbf798660661637064e97b936c82cda5f288387
iperever <iperever@cisco.com>
2020-09-20 13:23:09 +0200

    Repository

---
+++ src/main/java/com/example/filestorage/repository/FileRepository.java
@@ -0,0 +1,24 @@
+package com.example.filestorage.repository;
+
+import com.example.filestorage.entity.Record;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.sql.Timestamp;
+
+@Repository
+public interface FileRepository extends PagingAndSortingRepository<Record, Long> {
+
+    @Query(value = "from Record r where r.updatedTimeStamp BETWEEN ?1 AND ?2")
+    Page<Record> findAllRecordsByUpdateTimeStampBetween(@Param("fromDate") Timestamp fromDate,
+                                                        @Param("toDate") Timestamp toDate, Pageable pageable);
+
+    @Modifying
+    @Query("delete from Record r where r.id = ?1")
+    void deleteById(Long id);
+}
