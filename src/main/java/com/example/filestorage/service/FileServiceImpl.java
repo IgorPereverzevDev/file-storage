8ccb055901b2483473ca7fb415213d8f685ff5d6
iperever <iperever@cisco.com>
2020-09-20 13:24:54 +0200

    Service

---
+++ src/main/java/com/example/filestorage/service/FileServiceImpl.java
@@ -0,0 +1,73 @@
+package com.example.filestorage.service;
+
+import com.example.filestorage.constant.FileConstant;
+import com.example.filestorage.entity.Record;
+import com.example.filestorage.repository.FileRepository;
+import com.example.filestorage.utils.Validator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.sql.Timestamp;
+import java.util.Optional;
+
+
+@Service
+public class FileServiceImpl implements FileService {
+
+    private final FileRepository fileRepository;
+
+    private final Logger logger = LoggerFactory.getLogger(FileServiceImpl.class);
+
+    @Autowired
+    public FileServiceImpl(FileRepository fileRepository) {
+        this.fileRepository = fileRepository;
+    }
+
+    @Override
+    public void uploadFile(MultipartFile file) {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(file.getInputStream(), StandardCharsets.UTF_8))) {
+            String line;
+            reader.readLine();
+            while ((line = reader.readLine()) != null && Validator.isValid(line)) {
+                String[] data = line.split(FileConstant.SEPARATOR);
+                if (isValidRecord(data)) {
+                    Record record = new Record(Long.parseLong(data[0]), data[1], data[2], Timestamp.valueOf(data[3]));
+                    fileRepository.save(record);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            logger.info("Invalid data format");
+        }
+    }
+
+    @Override
+    public Optional<Record> getRecord(String id) {
+        return fileRepository.findById(Long.parseLong(id));
+    }
+
+    @Override
+    public Page<Record> getRecordsByDateBetween(String fromDate, String toDate, int page, int size) {
+        return fileRepository.findAllRecordsByUpdateTimeStampBetween(
+                Timestamp.valueOf(fromDate), Timestamp.valueOf(toDate), PageRequest.of(page, size));
+    }
+
+    @Override
+    public void removeRecord(String id) {
+        fileRepository.deleteById(Long.parseLong(id));
+    }
+
+    private boolean isValidRecord(String[] data) {
+        return Validator.isValidId(data[0]) && Validator.isValidName(data[1]) && Validator.isValidTimeStamp(data[3]);
+    }
+
+}
