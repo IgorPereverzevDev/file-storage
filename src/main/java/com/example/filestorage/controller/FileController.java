3a0a74a22258060c172a87523a159281551468d1
iperever <iperever@cisco.com>
2020-09-21 18:50:57 +0200

    Code Refactoring

--- src/main/java/com/example/filestorage/controller/FileController.java
+++ src/main/java/com/example/filestorage/controller/FileController.java
@@ -51,7 +51,7 @@ public class FileController {
             return new ResponseEntity<>("Incorrect timeFrom: " + timeFrom, HttpStatus.BAD_REQUEST);
         } else if (!Validator.isValidTimeStamp(timeTo)) {
             return new ResponseEntity<>("Incorrect timeTo: " + timeTo, HttpStatus.BAD_REQUEST);
-        }else if(Timestamp.valueOf(timeFrom).after(Timestamp.valueOf(timeTo))){
+        } else if (Timestamp.valueOf(timeFrom).after(Timestamp.valueOf(timeTo))) {
             return new ResponseEntity<>("timeFrom must be less than timeTo or equal", HttpStatus.BAD_REQUEST);
         }
         return new ResponseEntity<>(fileService.getRecordsByTimeStampBetweenTimeFromAndTimeTo(timeFrom, timeTo, page, size), HttpStatus.OK);
